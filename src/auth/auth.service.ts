194f49ff47279f74831b90ff6ca1bea175fea88e
Marouane Souda <marsou431@gmail.com>
2021-11-13 10:53:29 +0100

    feat: set UserRepository for dependency injection in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {}
+}
