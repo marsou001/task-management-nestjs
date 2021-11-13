478ae0e41a5491ee4a62e7b8a409c0b9138dcbcf
Marouane Souda <marsou431@gmail.com>
2021-11-13 13:00:22 +0100

    feat: handle duplicate usernames

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -11,7 +12,14 @@ export class UserRepository extends Repository<User> {
         user.username = username;
         user.password = password;
 
-        await user.save();
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === "23505") {
+                throw new ConflictException("username already exists!");
+            }
+            throw new InternalServerErrorException();
+        }
 
         return user;
     }
