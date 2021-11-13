ef3bf8bc81d737ceaae581eb8327d75075cb00e8
Marouane Souda <marsou431@gmail.com>
2021-11-13 12:42:22 +0100

    feat: add validation to auth credentials dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -8,7 +8,7 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<User> {
         return this.authService.signUp(authCredentialsDto);
     }
 }
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)  
     username: string;
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)    
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character!' })
     password: string;
 }
