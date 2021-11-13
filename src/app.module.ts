1e5c69fb11b9a5ae405d05e5c4452c26659ceed7
Marouane Souda <marsou431@gmail.com>
2021-11-13 10:42:01 +0100

    feat: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { TaskModule } from './task/task.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(typeOrmConfig), TasksModule, TaskModule, AuthModule],
+  controllers: [AuthController],
 })
 export class AppModule {}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: process.env.DATABASE_TYPE as 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST.toString(),
+    port: parseInt(process.env.DATABASE_PORT.toString()),
+    username: process.env.DATABASE_USERNAME.toString(),
+    password: process.env.DATABASE_PASSWORD.toString(),
+    database: process.env.DATABASE_NAME.toString(),
     autoLoadEntities: true,
     synchronize: true,
 };
