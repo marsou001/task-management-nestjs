4468efe56ccde105451b6ed9fddeae71f75a46a6
Marouane Souda <marsou431@gmail.com>
2021-11-14 11:05:30 +0100

    feat: create User custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Post, UseGuards, ValidationPipe, Req } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -16,11 +15,5 @@ export class AuthController {
     @Post('signin')
     signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
         return this.authService.signIn(authCredentialsDto);
-    }
-
-    @Post('test')
-    @UseGuards(AuthGuard())
-    test(@Req() req) {
-        console.log(req)
     }
 }
